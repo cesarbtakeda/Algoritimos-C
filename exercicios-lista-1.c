581aeda13bfadc2c3f3d061f3b1730dfa6c99a28
Cesar Augusto <114227831+cesarbtakeda@users.noreply.github.com>
2025-09-04 15:56:37 -0300

    Create exercicios-lista-1.c

---
+++ exercicios-lista-1.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<math.h>
+int main (void) {
+
+/*
+//ex1 lista 1
+int a=0;printf("\n Digite o numero: ");scanf("%i",&a);a=(a*2); printf("\n O valor do dobro eh: [%d]",a);
+*/
+
+/*
+//ex2 lista1
+float a=0,b=0,m=0;printf("\n Digite dois valores: ");scanf("%f%f",&a,&b);m=(a/b);printf("\n A media dos dois valores eh: [%.2f]",m);
+
+*/
+
+/*
+//ex3 lista1
+int a=0,b=0;printf("\n Digire um valor em metros para ser transformado em CM: ");scanf("%i",&a);b=(a*100);printf("\n O valor digita em metros em cm eh: [%d]",b);
+*/
+
+/*
+//ex4 lista1
+int a=0,b=0;printf("\n Digite sua idade: ");scanf("%i",&a);a=(a*365);printf("\n Sua idade em dias eh: [%d]",a);
+*/
+
+/*
+//ex5 lista 
+int a=0,b=0,c=0;printf("\n Digite dois valores: ");scanf("%i%i",&a,&b);c=(a/b);printf("O resto da sua divisao eh: [%d], e o resultado da divisao eh: [%d]",b,c);
+*/
+
+/*
+//ex6 lista 1
+int a=0,b=0,c=0;printf("Digite a base e a altura do triangulo: ");scanf("%i%i",&a,&b);c=(a*b)/2;printf("\n A area do triangulo eh: [%d]",c);
+*/
+
+/*
+//ex 7 lista 1
+float a=0,c=0;printf("\n Digite a temperatura em Fahrenheit para convertela para celsius: ");scanf("%f",&a);c=(a-32)/1.8;printf("\n A temperatua em celcius eh de: [%.2f]",c);
+	*/
+
+/*
+//ex8 lista 1	
+float c=0,a=0;printf("Digite o raio do circulo para exibir o perimetro dele: ");scanf("%f",&a);c=(2*3.14*a);printf("\n O seu perimetro eh de [%.2f]",c);
+*/
+	
+/*	
+//ex9 lista 1	
+float d=0,c=0,b=0,a=0;	printf("\n Digite um numero para calcular sua raiz, e digite outro numero para calcular sua oitava potencia: ");scanf("%f%f",&a,&b);c=sqrt(a);printf("\n A raiz do numero eh: [%.2f]",c);d=pow(b,8);printf("\ A sua oitva potencia eh [%.2f]",d);
+*/
+
+/*
+//ex10 lista 1
+float m=0,a=0,b=0;printf("\n Digite a distancia percorrido, e o consumo medio de combustivel, para calcular o consumo de conbultivel gasto: ");scanf("%f%f",&a,&b);m=(a/b);printf("\n O consumo medio de combustivel eh de: [%.2f] Listro(s) por kilometro(s)",m);
+*/
+
+/*
+//ex11 lista 1
+float m=0,a=0,b=0;printf("\n Digite a distancia percorrida e digite o tempo que demorou para calcular a velocidade media: ");scanf("%f%f",&a,&b);m=(a/b);printf("\n A velocidade media eh de [%.2f]",m);
+*/
+
+/*
+//ex12 lista 1
+float a=0,b=0,c=0;printf("\n Digite quantas horas voce trabalhou para calcular seu salario, quantas horas foram trabalhadas, e suas horas extras: ");scanf("%f%f",&a,&b);a=(a*10.00);b=(b*15.00);c=(a+b);printf("\n O total do seu salario eh de: [%.2f]",c);
+*/
+
+/*
+//ex13 lista 1
+float a=0,b=0;printf("\n Digite o numero de dias trabalhado, para calcular o valor do seu salario: ");scanf("%f",&a);b=(a/100)*8;a=(a-b);printf("\n O valor eh: [%.2f]",a);
+*/
+
+/*
+//ex14 lista 1
+float a=0,b=0; printf("\n Digite as duas notas, prova teorica e prova pratica: ");scanf("%f%f",&a,&b);a=(a/100)*60;b=(b/100)*40;a=(a+b);printf("\n A media do aluno eh: [%.2f]",a);
+*/
+
+	return 0;
+}
